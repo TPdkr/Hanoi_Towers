6f5798c4ec1efffafdb424604e47e133ef240c51
TPdkr <tpodko@gmail.com>
2024-03-14 17:22:15 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+package org.example
+
+fun main() {
+    val game=TowersService()//we have a game
+    game.play()//here we actually initialise it through play function
+}
---
+++ src/main/kotlin/towerout.kt
@@ -0,0 +1,58 @@
+package org.example
+//this function transfers a disk value to a string
+fun disktoline(disk:Int, maxwidth:Int):String{
+    val newline=StringBuilder()
+    newline.append(" ")//border
+    if(disk>0){
+        //we have a disk
+        newline.append(" ".repeat(maxwidth-disk))
+        newline.append("█".repeat(2*disk+1))
+        newline.append(" ".repeat(maxwidth-disk))
+    }else{
+        //we have no disk value 0
+        newline.append(" ".repeat(maxwidth))
+        newline.append("|")
+        newline.append(" ".repeat(maxwidth))
+    }
+    newline.append(" ")//border
+    return newline.toString()
+}
+//this function creates a string with tower bases
+fun base(disk:Int):String{
+    val newline=StringBuilder()
+    newline.append(" ")//border
+    for(i in 0..2){
+        newline.append("_".repeat(disk))
+        newline.append("┴")//center
+        newline.append("_".repeat(disk))
+        newline.append("  ")
+    }
+    return newline.toString()
+}
+//this function creates a string with indexes
+fun labels(disk:Int):String{
+    val newline=StringBuilder()
+    newline.append("^".repeat(disk))//first tower
+    newline.append("[1]")
+    newline.append("^".repeat(disk))
+    for(i in 1..2){//the other 2 towers
+        newline.append(" ".repeat(disk))
+        newline.append("[${i+1}]")
+        newline.append(" ".repeat(disk))
+        //newline.append(" ")
+    }
+    return newline.toString()
+}
+fun output(){
+    val sidemaxwidth=towers.height//how far from the tower center can we go
+    for(i in 0 until towers.height){//for each layer
+        val newline=StringBuilder()
+        for(j in 0 until 3){
+            newline.append(disktoline(towers.twrs[j].disks[i],sidemaxwidth))
+        }
+        println(newline.toString())
+    }
+    println(base(sidemaxwidth))//base
+    println(labels(sidemaxwidth))//labels
+
+}
